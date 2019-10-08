8b708696564d548f787ab321c7b8e0d413b23c5b
techrabbit58 <dzb@pruefbit.de>
2019-10-08 22:27:26 +0200

    US1 new test

--- src/test/java/de/pruefbit/kata/UserStory1Test.java
+++ src/test/java/de/pruefbit/kata/UserStory1Test.java
@@ -1,19 +1,30 @@
 package de.pruefbit.kata;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserStory1Test {
-    private static final int PRODUCTION_LIMIT = 10;
+    private static final int PRODUCTION_LIMIT = 3;
+    private static final List<String> FAMILIES = Arrays.asList("McFly", "Kojak", "Miller");
+
+    private BlockingQueue<String> cargoList;
+    private SantasSleigh theSleigh;
+
+    @BeforeEach
+    void setupEach() {
+        cargoList = new LinkedBlockingQueue<>();
+        theSleigh = new Sleigh(cargoList);
+    }
 
     @Test
     void one_toy_machine_runs_with_one_elf_on_defaults() {
-        BlockingQueue<String> cargoList = new LinkedBlockingQueue<>();
-        SantasSleigh theSleigh = new Sleigh(cargoList);
         ToyMachine tm = new ToyMachine();
         Elf elf = new Elf(tm::givePresent, theSleigh::pack, this::callback);
         elf.run();
@@ -22,14 +33,22 @@ class UserStory1Test {
 
     @Test
     void one_toy_machine_runs_with_one_elf_with_no_fam_plan() {
-        BlockingQueue<String> cargoList = new LinkedBlockingQueue<>();
-        SantasSleigh theSleigh = new Sleigh(cargoList);
         ProductionPlan pp = new ProductionPlan();
         pp.setProductionLimit(PRODUCTION_LIMIT);
         ToyMachine tm = new ToyMachine(pp);
         Elf elf = new Elf(tm::givePresent, theSleigh::pack, this::callback);
         elf.run();
         assertEquals(PRODUCTION_LIMIT, cargoList.size());
+    }
+
+    @Test
+    void one_to_one_works_with_a_family_plan() {
+        ProductionPlan pp = new ProductionPlan();
+        FAMILIES.forEach(f -> pp.setProductionLimit(f, PRODUCTION_LIMIT));
+        ToyMachine tm = new ToyMachine(pp);
+        Elf elf = new Elf(tm::givePresent, theSleigh::pack, this::callback);
+        elf.run();
+        assertEquals(PRODUCTION_LIMIT * FAMILIES.size(), cargoList.size());
     }
 
     private void callback(Elf elf) {
